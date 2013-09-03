fa983ddcb1cdfbb29c4592f7962eb7d7b216fbd2
Gabriela Rodriguez <gabelula@gmail.com>
2013-09-03 12:20:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leyes::Application.config.secret_key_base = 'c29bd7d3a32f739de7db29b3db5f78babb58b6379899e5e1fdaf284df2b757aa9c23e8954afb4f8574fd9041611dbe53761435c26eb6487ab504b3784b7a4196'
